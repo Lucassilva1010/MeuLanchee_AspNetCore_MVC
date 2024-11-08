a22ff7c220edc257a9f7ff026843589fbaaa7460
Lucas Augusto <lucasaugusto.simoes@gmail.com>
2024-11-08 18:43:19 -0300

    Adicionar arquivos de projeto.

---
+++ MeuLanchee/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using MeuLanchee.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuLanchee.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MeuLanchee/Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace MeuLanchee;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddAuthorization(); // Adiciona o serviço de autorização
+    }
+    public void configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+      app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+               
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+
+}
+
