123713a68129ec37cc9438a3bf9bc05320caa384
Lucas Augusto <lucasaugusto.simoes@gmail.com>
2024-11-11 05:30:56 -0300

    Feat(Models) - criação das classes de Lanches e Categoria

---
+++ MeuLanchee/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿namespace MeuLanchee.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+
+        public string Descricao { get; set; }
+
+        //Definição do relacionamento muito para Muitos
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ MeuLanchee/Models/Lanche.cs
@@ -0,0 +1,19 @@
+﻿namespace MeuLanchee.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Name { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailrUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //Definição do relacionamento entre as classes (Propriedades de navegação)
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
